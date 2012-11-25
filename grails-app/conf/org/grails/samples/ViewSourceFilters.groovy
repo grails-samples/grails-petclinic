3ff42331f621419087e33dbeeb4c6b9ada6c7cf5
Burt Beckwith <beckwithb@vmware.com>
2012-11-25 15:56:18 -0500

    fix for https://github.com/grails-samples/grails-petclinic/issues/1 - check that the model isn't null

--- grails-app/conf/org/grails/samples/ViewSourceFilters.groovy
+++ grails-app/conf/org/grails/samples/ViewSourceFilters.groovy
@@ -7,6 +7,10 @@ class ViewSourceFilters {
 	def filters = {
 		all(controller:'*', action:'*') {
 			after = { Map model ->
+				if (model == null) {
+					// will be null when redirecting
+					return
+				}
 				def controllerClass = grailsApplication.getArtefactByLogicalPropertyName("Controller", controllerName)
 				model.controllerClass = controllerClass.clazz.name
 				model.viewPath = "$controllerName:${model.viewName ?: actionName}"
