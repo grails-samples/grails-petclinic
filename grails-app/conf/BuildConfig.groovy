5875d2328b10ddb1c9141599bd199a72f5ffde29
Sebastian Vilstrup <sev@nineconsult.dk>
2014-03-22 19:37:04 +0100

    Updated tomcat and hibernate to match 2.3.7

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -20,9 +20,9 @@ grails.project.dependency.resolution = {
 	dependencies {}
 
 	plugins {
-		runtime ":hibernate:$grailsVersion"
+		build ':tomcat:7.0.52.1'
+		runtime ':hibernate:3.6.10.10' 
 		runtime ":jquery:1.8.0", ":jquery-ui:1.8.24"
 		runtime ":resources:1.1.6"
-		build ":tomcat:$grailsVersion"
 	}
 }
