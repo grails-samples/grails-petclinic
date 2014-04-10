00c538d8d2645eccfbf9bf85b64277ce9a478dfd
Stuart Williams <stuart@sidereal.ca>
2014-04-09 23:25:13 -0400

    Fixes #8, missing jquery-ui include

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -64,6 +64,7 @@ grails.project.dependency.resolution = {
         runtime ":hibernate:3.6.10.9" // or ":hibernate4:4.3.4"
         runtime ":database-migration:1.3.8"
         runtime ":jquery:1.11.0.2"
+        runtime ":jquery-ui:1.10.3"
         runtime ":resources:1.2.7"
         // Uncomment these (or add new ones) to enable additional resources capabilities
         //runtime ":zipped-resources:1.0.1"
