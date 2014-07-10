2ba67201eeeba9aad06dfcb9ae605e79ddd8b55f
malston <mark_alston@yahoo.com>
2014-07-10 07:32:21 -0600

    Added spring-cloud datasource config for mysql service

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,3 +1,13 @@
+import org.springframework.cloud.CloudFactory
+
+def cloud
+
+try {
+    cloud = new CloudFactory().cloud
+} catch(e) {
+    println e.getMessage() +  '... because we are not running in the cloud'
+}
+
 dataSource {
 	pooled = true
 	driverClassName = 'org.h2.Driver'
@@ -7,7 +17,7 @@ dataSource {
 hibernate {
 	cache.use_second_level_cache = true
 	cache.use_query_cache = false
-	cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory'
+	cache.region.factory_class = 'org.hibernate.cache.ehcache.EhCacheRegionFactory'
 	format_sql = true
 	use_sql_comments = true
 }
@@ -31,19 +41,59 @@ environments {
 	}
 	production {
 		dataSource {
-			dbCreate = 'update'
-			url = 'jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000'
 			pooled = true
-			properties {
-				maxActive = -1
-				minEvictableIdleTimeMillis=1800000
-				timeBetweenEvictionRunsMillis=1800000
-				numTestsPerEvictionRun=3
-				testOnBorrow=true
-				testWhileIdle=true
-				testOnReturn=true
-				validationQuery='SELECT 1'
+			dbCreate = 'update'
+
+			if (cloud) {
+				def dbInfo = cloud.getServiceInfo('petclinic-mysql')
+				driverClassName = 'com.mysql.jdbc.Driver'
+				url = dbInfo.jdbcUrl
+				username = dbInfo.userName
+				password = dbInfo.password
+			} else {
+				driverClassName = 'org.h2.Driver'
+				url = 'jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000'
+				properties {
+					maxActive = -1
+					minEvictableIdleTimeMillis=1800000
+					timeBetweenEvictionRunsMillis=1800000
+					numTestsPerEvictionRun=3
+					testOnBorrow=true
+					testWhileIdle=true
+					testOnReturn=true
+					validationQuery='SELECT 1'
+				}
 			}
 		}
+//		grails {
+//			mongo {
+//				if (cloud) {
+//					def mongoInfo = cloud.getServiceInfo('petclinic-mongodb')
+//					host = mongoInfo.host
+//					port = mongoInfo.port
+//					databaseName = mongoInfo.database
+//					username = mongoInfo.userName
+//					password = mongoInfo.password
+//				} else {
+//					host = 'localhost'
+//					port = 27107
+//					databaseName = 'foo'
+//					username = 'user'
+//					password = 'password'
+//				}
+//			}
+//			redis {
+//				if (cloud) {
+//					def redisInfo = cloud.getServiceInfo('petclinic-redis')
+//					host = redisInfo.host
+//					port = redisInfo.port
+//					password = redisInfo.password
+//				} else {
+//					host = 'localhost'
+//					port = 6379
+//					password = 'password'
+//				}
+//			}
+//		}
 	}
 }
